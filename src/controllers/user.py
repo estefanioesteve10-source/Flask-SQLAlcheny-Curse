3708cc1aa4217d72cc7edb4287134c4e0d759f5c
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-02-15 21:46:20 +0000

    add function _list_users

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from flask import Blueprint, request
+
 # importando recursos do app.py
 from src.app import User, db
 
@@ -13,10 +14,23 @@ def _create_user():
     db.session.add(user)                         # escrevendo o user no db
     db.session.commit()
 
+# Listando os usuarios
+def _list_users():
+    # users = db.session.execute(db.select(User)).scalars().all()
+    query = db.select(User)
+    users = db.session.execute(query).scalars().all()
+    return [
+        {
+            "id": user.id,
+            "username": user.username,
+        }
+        for user in users
+    ]
+
 @app.route('/', methods=['GET', 'POST'])
 def handle_user():
     if request.method == 'POST':
         _create_user()
         return {'message': 'User created!'}, HTTPStatus.CREATED
     else:
-        return {'users': [] }
+        return {'users': _list_users() }
