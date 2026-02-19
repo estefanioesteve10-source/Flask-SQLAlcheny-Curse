55dddda1b3e522fc935413289e7930f30daf2983
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-02-19 23:21:35 +0000

    adding or using pytest-mocker

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
-from unittest.mock import Mock, patch
 
 import pytest
+
 from src.controllers.utils import eleva_quadrado, requires_role
 
 
@@ -21,38 +21,32 @@ def test_eleva_quadrado_falha(test_input,exc_class, msg):
         eleva_quadrado(test_input)
     assert str(exc.value) == msg
 
-def test_requires_role_sucess():
-    mock_user = Mock()
+
+def test_requires_role_sucess(mocker):
+    # Given
+    mock_user = mocker.Mock()
     mock_user.role.name = 'admin'
-    mock_get_jwt_identity = patch('src.controllers.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.controllers.utils.db.session.get', return_value=mock_user)
-
-    mock_get_jwt_identity.start()
-    mock_db_get_or_404.start()
-
+    mocker.patch('src.controllers.utils.get_jwt_identity')
+    mocker.patch('src.controllers.utils.db.session.get', return_value=mock_user)
     decorated_function = requires_role('admin')(lambda: 'Sucess')
 
+    # When
     result = decorated_function()
 
+    # Then
     assert result == 'Sucess'
 
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
 
-def test_requires_role_fail():
-    mock_user = Mock()
+def test_requires_role_fail(mocker):
+    # Given
+    mock_user = mocker.Mock()
     mock_user.role.name = 'normal'
-    mock_get_jwt_identity = patch('src.controllers.utils.get_jwt_identity')
-    mock_db_get_or_404 = patch('src.controllers.utils.db.session.get', return_value=mock_user)
-
-    mock_get_jwt_identity.start()
-    mock_db_get_or_404.start()
-
+    mocker.patch('src.controllers.utils.get_jwt_identity')
+    mocker.patch('src.controllers.utils.db.session.get', return_value=mock_user)
     decorated_function = requires_role('admin')(lambda: 'Sucess')
 
-
+    # When
     result = decorated_function()
-    assert result == ({'message': 'Required role: admin'}, HTTPStatus.FORBIDDEN)
 
-    mock_get_jwt_identity.stop()
-    mock_db_get_or_404.stop()
+    # Then
+    assert result == ({'message': 'Required role: admin'}, HTTPStatus.FORBIDDEN)
