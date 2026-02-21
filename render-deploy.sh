495666296fbfd79cc5fb7322d6f3e8da0e0df131
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-02-21 01:55:53 +0000

    upgrade of render-deploy.sh

--- render-deploy.sh
+++ render-deploy.sh
@@ -2,12 +2,6 @@
 # Sair imediatamente se um comando falhar
 set -e
 
-# 1. Instalar as dependências do requirements.txt
-pip install -r requirements.txt
+poetry run flask --app src.app db upgrad
 
-# 2. Rodar as migrações do banco de dados (se você usa Flask-Migrate)
-flask --app src.app db upgrade
-
-# 3. Iniciar o servidor com Gunicorn
-# Nota: Se o seu arquivo de entrada for src/app.py, use src.app:app
-gunicorn src.app:app
+poetry run  gunicorn src.wsgi:app
